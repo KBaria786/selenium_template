e12399cb56039bb1de96688be7de56a55b42535e
KBaria <kshitijbbaria2000@gmail.com>
2023-09-27 12:24:12 +0530

    updates

--- src/test/java/com/automation/selenium_template/AllureTests.java
+++ src/test/java/com/automation/selenium_template/AllureTests.java
@@ -1,14 +1,7 @@
 package com.automation.selenium_template;
 
-import java.time.Duration;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import com.automation.selenium_template.driver.DriverControllerV3;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Allure;
 import io.qameta.allure.AllureLifecycle;
 import io.qameta.allure.model.Status;
@@ -46,16 +39,6 @@ public class AllureTests {
 		Allure.step("Test over", () -> {
 			Allure.step("Exception occured", () -> {throw e;});
 		});
-	}
-	
-	@Test
-	public void driverControllerTest() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		DriverControllerV3 dc = new DriverControllerV3(driver);
-		ReportingUtil.setProvider(new AllureReportingProvider());
-		dc.get("Launch google", "https://google.com");
-		dc.waitForInvisibilityOfElement("step description", "name~q", Duration.ofSeconds(3));
 	}
 	
 }
